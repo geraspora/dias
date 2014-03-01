524606009899c9f3ff4b2692137937e20fae30af
Dennis Schubert <software@dsx.cc>
2014-03-01 01:11:14 +0100

    bye, rails.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Dias
-
-Dias is a tool to list all the old logos from Geraspora, which is changing from time to time.
--- config/initializers/sneakpeek_param.rb
+++
@@ -1,2 +0,0 @@
-# You can access unpublished logos by calling /sneakpeek?key=changeme
-Dias::Application.config.sneakpeek_param = "changeme"
